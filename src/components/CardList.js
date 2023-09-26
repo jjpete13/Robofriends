12572eccf7656c04d119ff6831f442b3d7600975
jjpete13 <140276076+jjpete13@users.noreply.github.com>
2023-09-25 19:59:42 -0500

    updated robofriends

---
+++ src/components/CardList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from './Card'
+
+
+const CardList = ({ Robots }) => {
+	return (
+	<React.StrictMode>
+  <div>
+    	{
+    		Robots.map((user, i) => {
+					return  (
+						<Card 
+							key={i} 
+							id={Robots[i].id} 
+							name={Robots[i].name} 
+							email={Robots[i].email}
+						/>
+					);
+  		 })
+  		}
+  	</div>
+  	</React.StrictMode>
+	);
+}
+
+export default CardList;
